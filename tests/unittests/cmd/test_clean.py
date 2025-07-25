ani-sinha/cloud-init

commit 34cb46e20f5d3f6117a873c679578ed067045ffe

feat: add callback for datasources to clean config changes (#6100)
Datasources may make a number of modifications to the various config files.
When the user issues 'cloud-init clean -c datasource' or
'cloudinit clean -c all' commands, the datasources should clean up the changes
they might have made to the config files. Individual data sources should decide
how to implement the clean callback.

The generic ds.clean() implementation is empty at the present moment.
Subsequent patches will add support for cleaning configs for various
datasources.

Signed-off-by: Ani Sinha <anisinha@redhat.com>

cloudinit/cmd/clean.py
@@ -96,6 +96,7 @@ def get_parser(parser=None):
             "all",
             "ssh_config",
             "network",
+            "datasource",
         ],
         default=[],
         nargs="+",
@@ -115,7 +116,7 @@ def remove_artifacts(init, remove_logs, remove_seed=False, remove_config=None):
     @param: remove_seed: Boolean. Set True to also delete seed subdir in
         paths.cloud_dir.
     @param: remove_config: List of strings.
-        Can be any of: all, network, ssh_config.
+        Can be any of: all, network, ssh_config, datasource.
     @returns: 0 on success, 1 otherwise.
     """
     init.read_cfg()
@@ -131,6 +132,11 @@ def remove_artifacts(init, remove_logs, remove_seed=False, remove_config=None):
     ):
         for conf in GEN_SSH_CONFIG_FILES:
             del_file(conf)
+    if remove_config and set(remove_config).intersection(
+        ["all", "datasource"]
+    ):
+
+        init.fetch().clean()
 
     if not os.path.isdir(init.paths.cloud_dir):
         return 0  # Artifacts dir already cleaned

tests/unittests/cmd/test_clean.py
@@ -8,8 +8,9 @@ import pytest
 import cloudinit.settings
 from cloudinit.cmd import clean
 from cloudinit.distros import Distro
+from cloudinit.sources import DataSource
 from cloudinit.stages import Init
-from cloudinit.util import ensure_dir, sym_link, write_file
+from cloudinit.util import del_file, ensure_dir, sym_link, write_file
 from tests.unittests.helpers import mock, wrap_and_call
 
 MyPaths = namedtuple("MyPaths", "cloud_dir")
@@ -195,6 +196,34 @@ class TestClean:
 
         for conf_path in TEST_GEN_NET_CONFIG_FILES:
             assert conf_path.exists() is True, f"file {conf_path} removed!"
+        assert 0 == retcode
+
+    def test_clean_datasource_conf(self, clean_paths, init_class):
+        """remove_config removes datasource files when True."""
+
+        ds_conf = clean_paths.tmpdir.join("/var/run/ds")
+        assert ds_conf.exists() is False, f"Unexpected {ds_conf}"
+        ensure_dir(os.path.dirname(ds_conf))
+        ds_conf.write("#generated by generic DataSource\nfoobar\n")
+        assert ds_conf.exists() is True, "{ds_conf} not written!"
+
+        def ds_clean():
+            print("deleting {ds_conf}")
+            del_file(ds_conf)
+
+        ds = mock.Mock(spec=DataSource)
+        ds.clean = ds_clean
+
+        def ds_fetch():
+            return ds
+
+        init_class.fetch = ds_fetch
+        retcode = clean.remove_artifacts(
+            init_class,
+            remove_logs=False,
+            remove_config=["datasource"],
+        )
+        assert ds_conf.exists() is False, f"Unexpected file {ds_conf}"
         assert 0 == retcode
 
     @pytest.mark.allow_all_subp

cloudinit/sources/__init__.py
@@ -1021,6 +1021,14 @@ class DataSource(CloudInitPickleMixin, metaclass=abc.ABCMeta):
         """
         return
 
+    def clean(self):
+        """
+        Called when the user issues 'cloud-init clean -c datasource'
+        command.
+        Individual data sources should implement their own cleanup handler.
+        """
+        return
+
 
 def normalize_pubkey_data(pubkey_data):
     keys = []

commit fd37f56283c711f3f417ad6b30d525a7fe813d22

clean: do not attempt to find datasource when cache has been cleaned
When `cloud-init clean` is run with or without additional options but without
`-s`, it removes everything except the seed directory from `/var/lib/cloud`.
When `-s` is passed with or without other options, it removes the seed directory
as well. A subsequent invocation of `cloud-init clean -c datasource` will fail
to find the current datasource as the cache has been cleaned. Therefore, when
cache is not present, do not try to find the datasource.

Fixes: 34cb46e20f5d3f ("feat: add callback for datasources to clean config changes (#6100)")
Signed-off-by: Ani Sinha <anisinha@redhat.com>

cloudinit/cmd/clean.py
@@ -8,10 +8,11 @@
 
 import argparse
 import glob
+import logging
 import os
 import sys
 
-from cloudinit import settings
+from cloudinit import settings, sources
 from cloudinit.distros import uses_systemd
 from cloudinit.log import log_util
 from cloudinit.net.netplan import CLOUDINIT_NETPLAN_FILE
@@ -25,6 +26,7 @@ from cloudinit.util import (
     write_file,
 )
 
+LOG = logging.getLogger(__name__)
 ETC_MACHINE_ID = "/etc/machine-id"
 GEN_NET_CONFIG_FILES = [
     CLOUDINIT_NETPLAN_FILE,
@@ -132,14 +134,34 @@ def remove_artifacts(init, remove_logs, remove_seed=False, remove_config=None):
     ):
         for conf in GEN_SSH_CONFIG_FILES:
             del_file(conf)
-    if remove_config and set(remove_config).intersection(
-        ["all", "datasource"]
-    ):
-
-        init.fetch().clean()
 
     if not os.path.isdir(init.paths.cloud_dir):
+        if remove_config and set(remove_config).intersection(
+            ["all", "datasource"]
+        ):
+            log_util.multi_log(
+                "Cache removed, datasource not found, nothing cleaned.",
+                log=LOG,
+                log_level=logging.WARNING,
+            )
         return 0  # Artifacts dir already cleaned
+
+    cache_path = os.path.join(init.paths.cloud_dir, "instance")
+    if (
+        remove_config
+        and set(remove_config).intersection(["all", "datasource"])
+        and (is_link(cache_path) or os.path.isdir(cache_path))
+    ):
+        try:
+            # without cache, we can't find the datasource.
+            init.fetch().clean()
+        except sources.DataSourceNotFoundException:
+            log_util.multi_log(
+                "No datasource found, nothing cleaned.",
+                log=LOG,
+                log_level=logging.WARNING,
+            )
+
     seed_path = os.path.join(init.paths.cloud_dir, "seed")
     for path in glob.glob("%s/*" % init.paths.cloud_dir):
         if path == seed_path and not remove_seed:

tests/unittests/cmd/test_clean.py
@@ -198,14 +198,56 @@ class TestClean:
             assert conf_path.exists() is True, f"file {conf_path} removed!"
         assert 0 == retcode
 
-    def test_clean_datasource_conf(self, clean_paths, init_class):
-        """remove_config removes datasource files when True."""
-
+    def test_clean_datasource_conf_without_cache(
+        self, clean_paths, init_class
+    ):
+        """remove_config does not remove datasource files when cache
+        is not present.
+        """
+        ensure_dir(clean_paths.cloud_dir)
         ds_conf = clean_paths.tmpdir.join("/var/run/ds")
         assert ds_conf.exists() is False, f"Unexpected {ds_conf}"
         ensure_dir(os.path.dirname(ds_conf))
         ds_conf.write("#generated by generic DataSource\nfoobar\n")
         assert ds_conf.exists() is True, "{ds_conf} not written!"
+
+        cache = clean_paths.cloud_dir.join("instance")
+        assert cache.exists() is False, f"unexpected {cache} present!"
+
+        def ds_clean():
+            print("deleting {ds_conf}")
+            del_file(ds_conf)
+
+        ds = mock.Mock(spec=DataSource)
+        ds.clean = ds_clean
+
+        def ds_fetch():
+            return ds
+
+        init_class.fetch = ds_fetch
+        retcode = clean.remove_artifacts(
+            init_class,
+            remove_logs=False,
+            remove_config=["datasource"],
+        )
+        assert ds_conf.exists() is True, f"file {ds_conf} was removed!"
+        assert 0 == retcode
+
+    def test_clean_datasource_conf_with_cache(self, clean_paths, init_class):
+        """remove_config removes datasource files when cache
+        is present.
+        """
+        ensure_dir(clean_paths.cloud_dir)
+        ds_conf = clean_paths.tmpdir.join("/var/run/ds")
+        assert ds_conf.exists() is False, f"Unexpected {ds_conf}"
+        ensure_dir(os.path.dirname(ds_conf))
+        ds_conf.write("#generated by generic DataSource\nfoobar\n")
+        assert ds_conf.exists() is True, "{ds_conf} not written!"
+
+        cache = clean_paths.cloud_dir.join("instance")
+        assert cache.exists() is False, f"unexpected {cache} present!"
+        ensure_dir(cache)
+        assert cache.exists() is True, "{cache} not created!"
 
         def ds_clean():
             print("deleting {ds_conf}")